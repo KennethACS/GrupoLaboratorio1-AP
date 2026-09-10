cacf7396cf3624b0f4d1ffbedaaa9bb6fa0223a3
KennethACS <00118026@uca.edu.sv>
2026-09-10 14:16:41 -0600

    fix/declaracion de funcion

--- ejercicio1.cc
+++ ejercicio1.cc
@@ -2,13 +2,17 @@
 
 struct Metabolismo
 {
-    int calorias;
+    int calorias; //variable contabilizadora de calorias
 }metabolismo;
 
+//funciones
+void ConsultarCalorias(int calorias); //paso por copia, no modifica el valor original
 
 int main(){
-    
-    
+    metabolismo.calorias = 350; //inicializar una variable con un valor
+    std::cout<<"Se ha inicializado el numero de calorias en: "<<metabolismo.calorias<<std::endl;
+
+    ConsultarCalorias(metabolismo.calorias);
 }
 
 void ConsultarCalorias(int calorias){ //paso por copia, no modifica el valor original
