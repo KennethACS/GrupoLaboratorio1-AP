d302a9594c0344ba78deaf3d5dbf649c57e5ab85
KennethACS <00118026@uca.edu.sv>
2026-09-10 14:07:44 -0600

    feat/ConsultarCalorias

--- ejercicio1.cc
+++ ejercicio1.cc
@@ -1,6 +1,17 @@
 #include <iostream>
 
-int main(){
+struct Metabolismo
+{
+    int calorias;
+}metabolismo;
 
+
+int main(){
     
+    
+}
+
+void ConsultarCalorias(int calorias){ //paso por copia, no modifica el valor original
+
+    std::cout<<"El numero de calorias actuales es: "<<calorias<<std::endl;
 }
